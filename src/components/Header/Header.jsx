3ac25c5f5f497ac1ef4bb0ba48cc81e53e5efe39
Rixen <rixennoct@rambler.ru>
2022-10-16 19:06:37 +0300

    Some components

---
+++ src/components/Header/Header.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import style from './Header.module.css';
+import logo from '../../assets/images/logo.png';
+
+const Header = () => {
+    return (
+        <header className={style.header}>
+            <img className={style.logo} src={logo} />
+        </header>
+    );
+}
+
+export default Header;
---
+++ src/components/Workspace/Workspace.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import style from './Workspace.module.css';
+
+const Header = () => {
+    return (
+        <div className={style.workspace}>
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Accusamus alias assumenda autem commodi dignissimos est incidunt magni necessitatibus, optio perspiciatis? Ab accusamus culpa eligendi molestias quae quasi sunt temporibus veniam.
+        </div>
+    );
+}
+
+export default Header;
