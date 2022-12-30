bcaceb0c7bc6396f7054912440040a9af35e6de5
Rixen <rixennoct@rambler.ru>
2022-12-30 22:25:56 +0300

    Footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,16 +4,22 @@ import WorkspacePageContainer from "./components/WorkspacePage/WorkspacePageCont
 import MainMenu from "./components/MainMenu/MainMenu";
 import {Route, Routes} from 'react-router-dom';
 import HelpPage from "./components/HelpPage/HelpPage";
+import React from "react";
 
-const App = (props) => {
+const App = () => {
     return (
         <div className="app-wrapper">
             <Header/>
-            <MainMenu/>
-            <Routes>
-                <Route path='/*' element={<WorkspacePageContainer/>}/>
-                <Route path='/help/' element={<HelpPage/>}/>
-            </Routes>
+            <div className="content">
+                <MainMenu/>
+                <Routes>
+                    <Route path='/*' element={<WorkspacePageContainer/>}/>
+                    <Route path='/help/' element={<HelpPage/>}/>
+                </Routes>
+            </div>
+            <footer>
+                <span className="footer-text">rxnnct 2022</span>
+            </footer>
         </div>
     );
 }
