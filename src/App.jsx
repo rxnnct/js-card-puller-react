e3daf90d325cb1cbca206f108cdce2c7915a3e64
Rixen <rixennoct@rambler.ru>
2022-12-30 22:34:36 +0300

    Header component removed

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,19 @@
 import './App.scss';
-import Header from "./components/Header/Header";
 import WorkspacePageContainer from "./components/WorkspacePage/WorkspacePageContainer";
 import MainMenu from "./components/MainMenu/MainMenu";
 import {Route, Routes} from 'react-router-dom';
 import HelpPage from "./components/HelpPage/HelpPage";
 import React from "react";
+import logo from "./assets/images/logo.png";
 
 const App = () => {
     return (
         <div className="app-wrapper">
-            <Header/>
+            <header>
+                <img className="logo" src={logo} alt="logo"/>
+                <span className="appName">JSCardPuller v.2</span>
+            </header>
+
             <div className="content">
                 <MainMenu/>
                 <Routes>
@@ -17,6 +21,7 @@ const App = () => {
                     <Route path='/help/' element={<HelpPage/>}/>
                 </Routes>
             </div>
+
             <footer>
                 <span className="footer-text">rxnnct 2022</span>
             </footer>
